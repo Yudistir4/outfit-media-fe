388d371585e312be5202cec5d06c4f046c0bd321
Bizcuit <yudistir4maulana@gmail.com>
2022-05-29 15:53:44 +0700

    chore: rootpath

--- src/services/Config.js
+++ src/services/Config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-export const rootPath = "http://localhost:5000/";
-// export const rootPath = "https://outfit-media.herokuapp.com/";
+// export const rootPath = "http://localhost:5000/";
+export const rootPath = "https://outfit-media.herokuapp.com/";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyAN0nUlg4ue39o07RDpk7vRSnL5kU5FCF4",
